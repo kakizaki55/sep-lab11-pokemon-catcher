ee34dd700ac415510511b0f2caf8aa13de61a2ef
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-21 13:28:07 -0700

    pokemon randomizer doesnt do duplicits anymore, added a an button to to render pokemon funciton

--- app.js
+++ app.js
@@ -11,22 +11,30 @@ function randomNumber(){
     return randomNum;
 }
 
+function renderPokemon(){
+    let randomNum1 = randomNumber();
+    let randomNum2 = randomNumber();
+    let randomNum3 = randomNumber();
 
+    while (randomNum1 === randomNum2 || randomNum2 === randomNum3 || randomNum1 === randomNum3){
+        randomNum1 = randomNumber();
+        randomNum2 = randomNumber();
+        randomNum3 = randomNumber();
+    }
 
+    
+    let pokemon1 = allPokemons[randomNum1];
+    pokemonContainer1.src = pokemon1.url_image;
+        
+    let pokemon2 = allPokemons[randomNum2];
+    pokemonContainer2.src = pokemon2.url_image;
+        
+    let pokemon3 = allPokemons[randomNum3];
+    pokemonContainer3.src = pokemon3.url_image;
+}
+submitButton.addEventListener('click', () => {
+    renderPokemon();
 
-let randomNum1 = randomNumber();
-let randomNum2 = randomNumber();
-let randomNum3 = randomNumber();
+});
 
-
-let pokemon1 = allPokemons[randomNum1];
-pokemonContainer1.src = pokemon1.url_image;
-
-let pokemon2 = allPokemons[randomNum2];
-pokemonContainer2.src = pokemon2.url_image;
-
-let pokemon3 = allPokemons[randomNum3];
-pokemonContainer3.src = pokemon3.url_image;
-
-console.log(pokemon1, pokemon2, pokemon3);
 
